9af0d7ec1a37a488028c9fa69996c35378f87e34
yueg <yuegang1992@163.com>
2015-07-05 17:33:38 +0800

    db design

---
+++ db/conn.py
@@ -0,0 +1,16 @@
+# -*- conding: utf-8 -*-
+__author__ = 'yueg'
+import os
+import _mysql as db
+
+class Singleton(object):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            orig = super(Singleton, cls)
+            cls._instance = orig.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+class mysqlConn(Singleton):
+    def __init__(self, dbName):
+        self.conn = db.connect(host="localhost", user="root", passwd="123456", db=dbName)
