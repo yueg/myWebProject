9f9135101e3964442f70a181d44aa7885d18af2b
yueg <yuegang1992@163.com>
2015-07-06 10:55:39 +0800

    controller design

---
+++ mySpider/controller/articleOperation.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+__author__ = 'yueg'
+
+import db.conn as dbConn
+import db.operatiron as dbOperation
+import time
+
+dbName = "VillageConstruction"
+table = 'article'
+class articleOper():
+    def __init__(self):
+        self.conn = dbConn.mysqlConn(dbName).conn
+        self.db = dbOperation.dbOperation(self.conn)
+
+    def saveArticleToDB(self, title = "", pos1 = "", pos2 = "", pos3 = "", content = "", article_time = "", url = ""):
+        if self.isArticleExists(title):
+            return False
+        indexList = ['title', 'pos1', 'pos2', 'pos3', 'content', 'article_time', 'update_time', 'url']
+        valueList = []
+        valueList.append(title)
+        valueList.append(pos1)
+        valueList.append(pos2)
+        valueList.append(pos3)
+        valueList.append(content)
+        valueList.append(article_time)
+        valueList.append(time.strftime('%Y-%m-%d %X', time.localtime()))
+        valueList.append(url)
+        try:
+            self.db.insertList(table, indexList, valueList)
+            return True
+        except:
+            return False
+
+    def isArticleExists(self, title):
+        articleInfo = self.getArticleInfoFromTitle(title)
+        if not articleInfo:
+            return False
+        else:
+            return True
+
+    def getArticleInfoFromTitle(self, title):
+        articleInfo = self.db.findEx(table, 'title', title)
+        return articleInfo
